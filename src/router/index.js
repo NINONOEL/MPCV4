bac1bb9a5017ae60cd439662e1004138dc605597
NINONOEL <nnmonsanto23@gmail.com>
2025-11-03 13:06:19 +0800

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
 import { auth, db } from "@/config/firebase"
 import { getDoc, doc } from "firebase/firestore"
 import { onAuthStateChanged } from "firebase/auth"
@@ -305,7 +305,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import tailwindcss from "tailwindcss"
-import autoprefixer from "autoprefixer"
-import path from "path"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 export default defineConfig({
-  base: "/",
   plugins: [vue()],
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: false,
   },
 })
